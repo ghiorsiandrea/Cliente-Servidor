a01e74f3582c383e82b7404b5f0be9416da5179f
ghiorsiandrea <andreaghiorsims@gmail.com>
2022-08-17 11:41:09 -0300

    Exercise One, Curso Pildoras Informaticas - Java. Streams I. Accediendo a ficheros. Lectura. Vídeo 152 -

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import streams.Myfilereader;
+
+import javax.imageio.IIOException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Myfilereader accediendo = new Myfilereader();
+        accediendo.lee();
     }
 }
+
+
--- src/streams/Myfilereader.java
+++ src/streams/Myfilereader.java
@@ -1,4 +1,27 @@
 package streams;
 
+import javax.imageio.IIOException;
+import java.io.*;
+import java.nio.charset.Charset;
+
+/**
+ * This class was created attending the course of Streams - Java - of "Pildoras Informaticas"
+ */
+
 public class Myfilereader {
+
+    public void readd() {
+        try {
+            FileReader entrada = new FileReader("/home/dalga/andrea/proyectos/readme.txt");
+            int c = 0;
+            while (c != -1) {
+                c = entrada.read();
+                char letra =(char) c;
+                System.out.print(letra);
+            }
+            entrada.close();
+        } catch (IOException e) {
+            System.out.println("No se ha encontrado el archivo");
+        }
+    }
 }
