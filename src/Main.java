2c738999cafae80c03f37bcd08a366f1126c5955
ghiorsiandrea <andreaghiorsims@gmail.com>
2022-08-18 15:33:47 -0300

    Exercise Five, Curso Pildoras Informaticas - Streams IV. Streams V. Escribiendo archivos Streams Byte II. Vídeo 156

--- src/Main.java
+++ src/Main.java
@@ -8,25 +8,26 @@ public class Main {
     public static void main(String[] args) {
 
         ReadingWrittingByte readingWrittingByte = new ReadingWrittingByte();
-        readingWrittingByte.readdd();
+        readingWrittingByte.writee(readingWrittingByte.readdd());
 
-//        Myfilereader accessing = new Myfilereader();
-//        accessing.readd();
-//
-//        Myfilewritter writting = new Myfilewritter();
-//        writting.write();
-//
-//        Myfilewritter writting2 = new Myfilewritter();
-//        writting2.writeNonExistentFile();
-//
-//        Myfilereaderbuffered accessing2 = new Myfilereaderbuffered();
-//        accessing2.readd();
-//
-//        Myfilewritterbuffered writting3 = new Myfilewritterbuffered();
-//        writting3.write();
-//
-//        Myfilewritterbuffered writting4 = new Myfilewritterbuffered();
-//        writting4.writeNonExistentFile();
+
+        Myfilereader accessing = new Myfilereader();
+        accessing.readd();
+
+        Myfilewritter writting = new Myfilewritter();
+        writting.write();
+
+        Myfilewritter writting2 = new Myfilewritter();
+        writting2.writeNonExistentFile();
+
+        Myfilereaderbuffered accessing2 = new Myfilereaderbuffered();
+        accessing2.readd();
+
+        Myfilewritterbuffered writting3 = new Myfilewritterbuffered();
+        writting3.write();
+
+        Myfilewritterbuffered writting4 = new Myfilewritterbuffered();
+        writting4.writeNonExistentFile();
 
 
     }
--- src/streams/readingwrittingbyte/ReadingWrittingByte.java
+++ src/streams/readingwrittingbyte/ReadingWrittingByte.java
@@ -3,21 +3,44 @@ package streams.readingwrittingbyte;
 import java.io.*;
 
 public class ReadingWrittingByte {
-    public void readdd() {
+    public int[] readdd() {
+        int contador = 0;
+        int datos_entrada[] = new int[93193];
         try {
-            FileInputStream readingwritting = new FileInputStream("/home/dalga/andrea/proyectos/Cliente-Servidor/streams/readingwritting/capture.jpg");
+            FileInputStream reading = new FileInputStream(
+                    "/home/dalga/andrea/proyectos/Cliente-Servidor/streams/readingwritting/capture.jpg");
             boolean final_ar = false;
             while (!final_ar) {
-                int byte_entrada = readingwritting.read();
-                if (byte_entrada == -1) {
+                int byte_entrada = reading.read();
+                if (byte_entrada != -1) {
+                    datos_entrada[contador] = byte_entrada;
+                } else {
                     final_ar = true;
-                    System.out.println(byte_entrada);
                 }
+                contador++;
+                System.out.println(byte_entrada);
             }
-
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+            System.out.println("Error al acceder a la imagen");
         }
 
+        return datos_entrada;
+    }
+
+    public void writee(int datos_nuevo_fichero[]) {
+
+        try {
+            FileOutputStream writting = new FileOutputStream(
+                    "/home/dalga/andrea/proyectos/Cliente-Servidor/streams/readingwritting/capture_COPIA.jpg");
+
+            for (int i = 0; i < datos_nuevo_fichero.length; i++) {
+                writting.write(datos_nuevo_fichero[i]);
+            }
+            writting.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error al crear la imagen");
+        }
     }
 }
