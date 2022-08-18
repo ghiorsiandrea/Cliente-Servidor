84dbf502828c46b326b93a4d8eabc8a675ad7bb7
ghiorsiandrea <andreaghiorsims@gmail.com>
2022-08-18 15:34:45 -0300

    Exercise Two, Curso Pildoras Informaticas - Streams II. Accediendo a ficheros Escritura. Vídeo 153 -

--- src/streams/single/Myfilewritter.java
+++ src/streams/single/Myfilewritter.java
@@ -10,7 +10,7 @@ public class Myfilewritter {
     public void write() {
         String frase = "Esta es una prueba de escritura";
         try {
-            FileWriter escritura = new FileWriter("/home/dalga/andrea/proyectos/Cliente-Servidor/streams/writeme.txt");
+            FileWriter escritura = new FileWriter("/home/dalga/andrea/proyectos/Cliente-Servidor/streams/single/writeme.txt");
             for (int i = 0; i < frase.length(); i++) {
                 escritura.write(frase.charAt(i));
             }
@@ -23,7 +23,7 @@ public class Myfilewritter {
     public void writeNonExistentFile() {
         String frase = "Esta es una prueba de escritura";
         try {
-            FileWriter escritura = new FileWriter("/home/dalga/andrea/proyectos/Cliente-Servidor/streams/writeme2.txt");
+            FileWriter escritura = new FileWriter("/home/dalga/andrea/proyectos/Cliente-Servidor/streams/single/writeme2.txt");
             for (int i = 0; i < frase.length(); i++) {
                 escritura.write(frase.charAt(i));
             }
