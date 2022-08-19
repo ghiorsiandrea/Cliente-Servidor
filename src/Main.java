e71099ad6ea3df97968a931d2c0662071dfa7b20
ghiorsiandrea <andreaghiorsims@gmail.com>
2022-08-19 17:14:53 -0300

    Comments were added

--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,24 @@ import streams.single.Myfilewritterbuffered;
 public class Main {
     public static void main(String[] args) {
 
-        ReadingWrittingByte readingWrittingByte = new ReadingWrittingByte();
-        readingWrittingByte.writee(readingWrittingByte.readdd());
 
+        // Exercise One, Curso Pildoras Informaticas - Java. Streams I.
+        // Accediendo a ficheros. Lectura. Vídeo 152 -
 
         Myfilereader accessing = new Myfilereader();
         accessing.readd();
+
+        //Exercise Two, Curso Pildoras Informaticas - Streams II.
+        // Accediendo a ficheros Escritura. Vídeo 153 -
 
         Myfilewritter writting = new Myfilewritter();
         writting.write();
 
         Myfilewritter writting2 = new Myfilewritter();
         writting2.writeNonExistentFile();
+
+        //Exercise Tree, Curso Pildoras Informaticas - Streams III.
+        // Usando buffers. Vídeo 154 -
 
         Myfilereaderbuffered accessing2 = new Myfilereaderbuffered();
         accessing2.readd();
@@ -28,6 +34,17 @@ public class Main {
 
         Myfilewritterbuffered writting4 = new Myfilewritterbuffered();
         writting4.writeNonExistentFile();
+
+        // Exercise Four, Curso Pildoras Informaticas - Streams IV.
+        // Leyendo archivos. Streams Byte I. Vídeo 155
+
+        //Exercise Five, Curso Pildoras Informaticas - Streams IV. Streams V.
+        // Escribiendo archivos Streams Byte II. Vídeo 156
+
+
+        ReadingWrittingByte readingWrittingByte = new ReadingWrittingByte();
+        readingWrittingByte.writee(readingWrittingByte.readdd());
+
 
 
     }
