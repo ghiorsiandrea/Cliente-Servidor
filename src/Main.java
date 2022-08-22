078b603d93b1ae1c358da3d53bb0569b58236188
ghiorsiandrea <andreaghiorsims@gmail.com>
2022-08-22 13:16:01 -0300

    We have to create another Main in order to organize in a better way the project

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,6 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
-        //Excersice Six, Curso Pildoras Informaticas - Eventos I. Vídeo 65
-
-        MyEvents myEvents = new MyEvents();
-        myEvents.setVisible(true);
-        myEvents.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
 
--- src/events/MyEvents.java
+++ src/events/MyEvents.java
@@ -1,6 +1,8 @@
 package events;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class MyEvents extends JFrame {
@@ -15,12 +17,19 @@ public class MyEvents extends JFrame {
 }
 
 // TERCER EVENTO - LA LAMINA ES EL LISTENER
-class LaminaBotones extends JPanel {
+class LaminaBotones extends JPanel implements ActionListener {
+
     //SEGUNDO EVENTO - OBJETO FUENTE
     JButton bluebutton = new JButton("Blue");
 
     public LaminaBotones() {
         // PRIMER EVENTO
         add(bluebutton);
+        bluebutton.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
