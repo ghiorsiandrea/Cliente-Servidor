b1a33ca8febadb6c177e51e5077024882668c503
ghiorsiandrea <andreaghiorsims@gmail.com>
2022-08-22 15:39:41 -0300

    Excersice Six, Curso Pildoras Informaticas - Eventos II. Vídeo 65

--- src/events/MyEvents.java
+++ src/events/MyEvents.java
@@ -16,20 +16,36 @@ public class MyEvents extends JFrame {
 
 }
 
-// TERCER EVENTO - LA LAMINA ES EL LISTENER
+// TERCER EVENTO - OBJETO LISTENER - LA LAMINA
 class LaminaBotones extends JPanel implements ActionListener {
 
     //SEGUNDO EVENTO - OBJETO FUENTE
     JButton bluebutton = new JButton("Blue");
+    JButton yellowbutton = new JButton("Yellow");
+    JButton redbutton = new JButton("Red");
 
     public LaminaBotones() {
-        // PRIMER EVENTO
+        // PRIMER EVENTO - OBJETO EVENTO
         add(bluebutton);
+        add(yellowbutton);
+        add(redbutton);
         bluebutton.addActionListener(this);
+        yellowbutton.addActionListener(this);
+        redbutton.addActionListener(this);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
+        Object buttonpressed = e.getSource();
+
+        if (buttonpressed == bluebutton) {
+            setBackground(Color.blue);
+        }
+        if (buttonpressed == yellowbutton) {
+            setBackground(Color.yellow);
+        } else {
+            setBackground(Color.red);
+        }
     }
 }
