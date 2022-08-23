592fd472bd867888870b6a9c17655f5f3dabf3ba
ghiorsiandrea <andreaghiorsims@gmail.com>
2022-08-23 12:44:48 -0300

    Excersice Seven, Curso Pildoras Informaticas - Eventos III. Vídeo 67

--- src/events/Main.java
+++ src/events/Main.java
@@ -5,10 +5,16 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
-        //Excersice Six, Curso Pildoras Informaticas - Eventos I. Vídeo 65
+        //Excersice Six, Curso Pildoras Informaticas - Eventos I - II. Vídeo 65 - 66
 
         MyEvents myEvents = new MyEvents();
         myEvents.setVisible(true);
         myEvents.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Excersice Seven, Curso Pildoras Informaticas - Eventos III. Vídeo 67
+
+        MyEvents2 myEvents2 = new MyEvents2();
+        myEvents2.setVisible(true);
+        myEvents2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
---
+++ src/events/MyEvents2.java
@@ -0,0 +1,54 @@
+package events;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class MyEvents2 extends JFrame {
+
+    public MyEvents2() {
+        setTitle("Buttons and Events2");
+        setBounds(700, 300, 500, 300);
+        LaminaBotones milaminaBotones = new LaminaBotones();
+        add(milaminaBotones);
+    }
+
+}
+
+// TERCER EVENTO - OBJETO LISTENER - LA LAMINA
+class LaminaBotones2 extends JPanel implements ActionListener {
+
+    //SEGUNDO EVENTO - OBJETO FUENTE
+    JButton bluebutton = new JButton("Blue");
+    JButton yellowbutton = new JButton("Yellow");
+    JButton redbutton = new JButton("Red");
+
+    public LaminaBotones2() {
+        // PRIMER EVENTO - OBJETO EVENTO
+        add(bluebutton);
+        add(yellowbutton);
+        add(redbutton);
+        bluebutton.addActionListener(this);
+        yellowbutton.addActionListener(this);
+        redbutton.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        Object buttonpressed = e.getSource();
+
+        if (buttonpressed == bluebutton) {
+            setBackground(Color.blue);
+        }
+
+        if (buttonpressed == yellowbutton) {
+            setBackground(Color.yellow);
+        }
+
+        if (buttonpressed == redbutton) {
+            setBackground(Color.red);
+        }
+    }
+}
