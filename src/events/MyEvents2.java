2bcc1eed674fbd57835fe2dec3574c9c457f60c8
ghiorsiandrea <andreaghiorsims@gmail.com>
2022-08-23 12:58:29 -0300

    Excersice Seven, Curso Pildoras Informaticas - Eventos III. Vídeo 67

--- src/events/MyEvents2.java
+++ src/events/MyEvents2.java
@@ -10,8 +10,8 @@ public class MyEvents2 extends JFrame {
     public MyEvents2() {
         setTitle("Buttons and Events2");
         setBounds(700, 300, 500, 300);
-        LaminaBotones milaminaBotones = new LaminaBotones();
-        add(milaminaBotones);
+        LaminaBotones2 milaminaBotones2 = new LaminaBotones2();
+        add(milaminaBotones2);
     }
 
 }
